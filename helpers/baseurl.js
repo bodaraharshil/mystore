e951daf0b3269d977ee6ec651cc42048b991ed98
bodaraharshil <developer.harshilbodara@gmail.com>
2021-02-14 13:26:14 +0530

    changed producation url

--- helpers/baseurl.js
+++ helpers/baseurl.js
@@ -1,2 +1,2 @@
-const BaseUrl = process.env.NODE_ENV === "producation" ? "https://mystore.vercel.app" :"http://localhost:3000";
+const BaseUrl = process.env.NODE_ENV === "producation" ? "https://mystorevisit.herokuapp.com/" :"http://localhost:3000";
 export default BaseUrl;
