ae3fd1e96b4999c94e7da5972bab625d440a5810
bodaraharshil <developer.harshilbodara@gmail.com>
2021-02-14 15:34:12 +0530

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8000 || 5000;
 const app = express();
 const cors = require('cors')
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '.next')));
 app.use(cors())
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '.next', ''));
