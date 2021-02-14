188dbeb8e31b139f1e7e0ae3b6483f7255dc2543
bodaraharshil <developer.harshilbodara@gmail.com>
2021-02-14 14:39:33 +0530

    add on heroku

--- helpers/baseurl.js
+++ helpers/baseurl.js
@@ -1,2 +1,2 @@
-const BaseUrl = process.env.NODE_ENV === "producation" ? "https://mystorevisit.herokuapp.com/" :"http://localhost:3000";
+const BaseUrl = process.env.NODE_ENV === "producation" ? "http://localhost:3000" :"http://localhost:3000";
 export default BaseUrl;
